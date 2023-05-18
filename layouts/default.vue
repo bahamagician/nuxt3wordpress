805e5329b222dbf93c9429c4cd48e2947653ba56
bahamagician <cthronebury@gmail.com>
2023-05-18 13:43:34 -0400

    Init repo

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Some default layout shared across all pages
+      <NuxtLink to="/">
+        Home page
+      </NuxtLink>
+      <NuxtLink to="/aboutUs">
+        About page
+      </NuxtLink>
+    <slot />
+  </div>
+</template>
---
+++ pages/aboutUs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    This is the about us page bwoy
+     <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+  const { data } = await useFetch('/api/hello');
+  console.log(data);
+</script>
